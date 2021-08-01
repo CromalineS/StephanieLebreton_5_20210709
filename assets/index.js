e5b3d4e28a3c8ca8eaf09757de99877a82e30581
Cromaline <stf@cromaline.fr>
2021-08-01 15:52:12 +0200

    modif globale = animation

--- assets/index.js
+++ assets/index.js
@@ -10,7 +10,7 @@ function accessData() {										//fonction de récupération des données du se
 function catalog(data) {									//fonction qui remplit le catalog avec les données récupérée
 	
 	console.log('Request successful', data);				//contrôle réussite de la requête dans la console et affichage des données récupérées = tableau contenant un tableau par produit				
-	
+/*	
 	for(const x of data) {									//pour chaque cellule du tableau
 
 		const catalog = document.getElementById('catalog');					//cible la div d'id 'catalog'
@@ -28,6 +28,29 @@ function catalog(data) {									//fonction qui remplit le catalog avec les donn
 			<div class="tiers">
 				<form action="product.html" class="button-right">
 					<input type="hidden" name="teddyId" value="${x._id}">
+					<input type="submit" value="Plus d'infos !" class="button">
+				</form>
+			</div>
+		`;
+	}
+*/
+	for(const i in data) {	//version avec indice pour gestion des animations //pour chaque cellule du tableau
+
+		const catalog = document.getElementById('catalog');					//cible la div d'id 'catalog'
+		const product = document.createElement('div');						//création d'une div pour afficher le produit
+		product.setAttribute('class', 'section section-horizontal-center anim anim-' + i);	//style de la div qui affiche le produit
+		catalog.append(product);											//ajoute une ligne au catalogue pour chaque produit ( for chaque élément of tableau data)
+		product.innerHTML =													//littéraux gabarit pour ajouter les infos de chaque produit = nom, vignette, une balide form contenant deux input (une cachée "contenant" l'id, une submit pour envoyer l'id dans l'url et aller à la page produit correspondante)
+		`
+			<div class="tiers">
+				<h3>${data[i].name}</h3>
+			</div>
+			<div class="tiers">
+				<img src="${data[i].imageUrl}" class="thumbnail-teddies">
+			</div>
+			<div class="tiers">
+				<form action="product.html" class="button-right">
+					<input type="hidden" name="teddyId" value="${data[i]._id}">
 					<input type="submit" value="Plus d'infos !" class="button">
 				</form>
 			</div>
